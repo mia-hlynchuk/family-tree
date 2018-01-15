20260077a36ed7a164077a12a19e1fc256139ae1
mia-hlynchuk <solomiya.hlynchuk@gmail.com>
2018-01-15 15:17:41 -0500

    Starting family tree

---
+++ src/Tree.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Tree.css';
+
+class Tree extends Component {
+  render() {
+    return (
+      <div className="Tree">
+        <header>
+          <h1>Моє Сімейне Дерево</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Tree;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Tree from './Tree';
+import './index.css';
+
+ReactDOM.render(<Tree />, document.getElementById('root'));
